a3951fdd76a0f5b0a8642935695c7234270b751f
hello112334 <hello112334@gmail.com>
2024-07-01 23:12:05 +0900

    delete 施設名称

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def delete_headers(df, line_number):
     """
     ヘッダー行を削除
     """
-    target_list = ["基本情報", "施設名", "施設名称"]
+    target_list = ["基本情報", "施設名"]
     for target in target_list:
         if df.iloc[0, 0] == target or (len(df.columns) > 1 and df.iloc[0, 1] == target):
             return df.drop(df.index[:line_number])
